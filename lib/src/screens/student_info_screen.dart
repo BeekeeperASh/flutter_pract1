f28ae619f6cd2cdb6abedc2c35e005b0a32369bd
Beekeeper <alvshinev@gmail.com>
2025-09-30 16:56:00 +0300

    Использование виджета Column

---
+++ lib/src/screens/student_info_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class StudentInfoPage extends StatelessWidget {
+  const StudentInfoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Информация студента'),
+      ),
+      body: const Center(child: StudentInfoCard()),
+    );
+  }
+}
+
+class StudentInfoCard extends StatelessWidget {
+  const StudentInfoCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(20),
+      elevation: 8,
+      child: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Информация о студенте',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.deepPurple,
+              ),
+            ),
+            const SizedBox(height: 30),
+            _buildInfoRow('ФИО:', 'Шинёв Алексей Вадимович'),
+            const SizedBox(height: 15),
+            _buildInfoRow('Номер группы:', 'ИКБО-06-22'),
+            const SizedBox(height: 15),
+            _buildInfoRow('Номер студенческого:', '22И1253'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildInfoRow(String label, String value) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          width: 180,
+          child: Text(
+            label,
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+              color: Colors.grey,
+            ),
+          ),
+        ),
+        Expanded(
+          child: Text(
+            value,
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+              color: Colors.red,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
